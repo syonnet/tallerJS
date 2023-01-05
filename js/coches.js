bc3e8ee3ceeed331e39480121e43be54a9c7aca4
syonnet <vic_gen@hotmail.com>
2023-01-04 22:14:10 -0500

    Initial commit

---
+++ js/coches.js
@@ -0,0 +1,20 @@
+function calcularDescuento() {
+    // Obtener el nombre del coche
+    var nombreCoche = document.getElementById("coche").value;
+  
+    // Establecer el descuento en 0 por defecto
+    var descuento = 0;
+  
+    // Si el nombre del coche es "ford fiesta", establecer el descuento en 5%
+    if (nombreCoche.toLowerCase() === "ford fiesta") {
+      descuento = 5;
+    }
+    // Si el nombre del coche es "ford focus", establecer el descuento en 10%
+    else if (nombreCoche.toLowerCase() === "ford focus") {
+      descuento = 10;
+    }
+  
+    // imprimir descuento
+    document.getElementById("resultado").innerHTML = "Tu descuento es del " + descuento + "%";
+  }
+  
---
+++ js/notas.js
@@ -0,0 +1,17 @@
+function calcularMedia() {
+    // Obtener los valores de las notas
+    var nota1 = document.getElementById("nota1").value;
+    var nota2 = document.getElementById("nota2").value;
+    var nota3 = document.getElementById("nota3").value;
+
+    // Calculo
+    var media = (parseInt(nota1) + parseInt(nota2) + parseInt(nota3)) / 3;
+
+    // Mostrar el resultado 
+    var resultadoDiv = document.getElementById("resultado");
+    if (media < 7) {
+      resultadoDiv.innerHTML = "Suspendido con una media de " + media;
+    } else {
+      resultadoDiv.innerHTML = "Aprobado con una media de " + media;
+    }
+  }
