5db6ef2123065a18043b21c1ae9a29b1039dd6b1
vic_gen <101438801+syonnet@users.noreply.github.com>
2023-01-04 23:59:49 -0500

    var

--- js/notas.js
+++ js/notas.js
@@ -1,14 +1,14 @@
 function calcularMedia() {
     // Obtener los valores de las notas
-    var nota1 = document.getElementById("nota1").value;
-    var nota2 = document.getElementById("nota2").value;
-    var nota3 = document.getElementById("nota3").value;
+    let nota1 = document.getElementById("nota1").value;
+    let nota2 = document.getElementById("nota2").value;
+    let nota3 = document.getElementById("nota3").value;
 
     // Calculo
-    var media = (parseInt(nota1) + parseInt(nota2) + parseInt(nota3)) / 3;
+    let media = (parseInt(nota1) + parseInt(nota2) + parseInt(nota3)) / 3;
 
     // Mostrar el resultado 
-    var resultadoDiv = document.getElementById("resultado");
+    let resultadoDiv = document.getElementById("resultado");
     if (media < 7) {
       resultadoDiv.innerHTML = "Reprobado con una media de " + media;
     } else {
